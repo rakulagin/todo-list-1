827fd335246e1a0711552d160cbf46c809ef004f
rakulagin <koulagin@gmail.com>
2022-06-22 23:28:28 +0400

    начал верстку, компонент списка

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import React from "react";
+import List from "./components/List/List";
+import listSvg from './assets/img/list.svg'
 
 function App() {
-    const we = "345"
   return (
-    <div className="App">
-      123
-        {we}
+    <div className="todo">
+        <div className="todo__sidebar">
+            <img src="" alt=""/>
+            <List items={[
+                {
+
+                    icon: <img src={listSvg} alt=""/>,
+                    name: 'Все задачи',
+                    active: true
+                }
+            ]}/>
+            <List items={[
+                {
+                    color: "green",
+                    name: 'Покупки'
+                },
+                {
+                    color: "blue",
+                    name: 'Фронтенд'
+                },
+                {
+                    color: "pink",
+                    name: 'Фильмы и сериалы'
+                }
+            ]}/>
+        </div>
+        <div className="todo__tasks">
+            фронтенд
+        </div>
     </div>
   );
 }
